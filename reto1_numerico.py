97d6c584d0e4ca6fe45a771009b349056a8223c9
Angello Villanueva <angellovillanuevaind@gmail.com>
2020-06-13 18:43:33 -0400

    Creación del nuevo archivo donde se resuelven los problemas matematicos

---
+++ reto1_numerico.py
@@ -0,0 +1,8 @@
+def suma(numero_1,numero_2):
+    sum= numero_1 +numero_2
+    print(sum)
+
+if __name__ == '__main__':
+    numero_1 = float(input('Escribe el primer numero: '))
+    numero_2 = float(input('Escribe el segundo numero: '))
+    suma(numero_1,numero_2)
