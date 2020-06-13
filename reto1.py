308d298568322e0ce005f6a03153ef50a3cd42b6
Angello Villanueva <angellovillanuevaind@gmail.com>
2020-06-13 18:17:05 -0400

    Creacion de la rama "pruebas",agregar el ejercicio 3 del reto 1

--- reto1.py
+++ reto1.py
@@ -3,5 +3,7 @@ def name():
     apellido = str(input('¿Cual es tu Apellido?: '))
     print('Hola, {} {}'.format(nombre, apellido))
 
+    print('Platzi cuenta con cursos de: \nDesarrollo e Ingenieria\nMarketing \nDiseño y UX \nNegocios y emprendimiento \nProducción Audiovisual \nCrecimiento profesional')
+    
 if __name__=='__main__':
     name()
