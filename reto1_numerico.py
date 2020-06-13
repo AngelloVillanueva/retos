9354237cda6d04a593cf18767273915bc24c3be7
Angello Villanueva <angellovillanuevaind@gmail.com>
2020-06-13 18:50:29 -0400

    Añadir funcion de suma y multiplicación ademas de cambiar nombre de sum a  resultado

--- reto1_numerico.py
+++ reto1_numerico.py
@@ -1,8 +1,14 @@
 def suma(numero_1,numero_2):
-    sum= numero_1 +numero_2
-    print(sum)
+    resultado = numero_1 +numero_2
+    print(resultado)
+
+def suma_multiplicacion(numero_1,numero_2,numero_3):
+    resultado_2 = (numero_1+numero_2)*numero_3
+    print(resultado_2)
 
 if __name__ == '__main__':
     numero_1 = float(input('Escribe el primer numero: '))
     numero_2 = float(input('Escribe el segundo numero: '))
+    numero_3 = float(input('Escribe el tercer numero: '))
     suma(numero_1,numero_2)
+    suma_multiplicacion(numero_1,numero_2,numero_3)
