0fece0c3078ec7009582e2b992c8d71cfcef7f3a
Angello Villanueva <angellovillanuevaind@gmail.com>
2020-06-13 18:36:37 -0400

    Terminamos de editar el contenido del reto 1

--- reto1.py
+++ reto1.py
@@ -5,5 +5,6 @@ def name():
 
     print('Platzi cuenta con cursos de: \nDesarrollo e Ingenieria\nMarketing \nDiseño y UX \nNegocios y emprendimiento \nProducción Audiovisual \nCrecimiento profesional')
     
+
 if __name__=='__main__':
     name()
