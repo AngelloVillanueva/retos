f9b21e8530081324607bb750ca7ed12629395a23
Angello Villanueva <angellovillanuevaind@gmail.com>
2020-06-14 16:11:45 -0400

    Se arreglo el reto numero 11 y se dejo concluido

--- reto_11.py
+++ reto_11.py
@@ -1,3 +1,11 @@
+def division(num_1,num_2):
+    resultado = num_1/num_2
+    print(f'{num_2} entra {resultado} veces en {num_1}')
+
 if __name__ == '__main__':
-    main()
-    
+    num_1 = float(input('Ingresa un numero mayor a 1000: '))
+    num_2 = float(input('Ingresa un numero menor a 100: '))
+    if num_1 >= 1000 and num_2 <=100:
+        division(num_1,num_2)
+    else:
+        print('Los numeros no coinciden')
