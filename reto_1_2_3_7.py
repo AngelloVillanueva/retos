4bd8a4124bfc2470b8fd67a35614667dcdd0d863
AngelloVillanueva <66492843+AngelloVillanueva@users.noreply.github.com>
2020-06-14 01:02:42 -0400

    Renombrar archivo para especificar retos

--- reto_1_2_3_7.py
+++ reto_1_2_3_7.py
@@ -4,8 +4,8 @@ def name():
     edad = int(input('¿Cual es tu edad: '))
     edad_ap = edad -1 
     edad_pa = edad +1
+    
     print('Hola, {} {}, el año pasado tenias {} años y el próximo cumpliras {} años'.format(nombre, apellido, edad_ap, edad_pa))
-
     print('Platzi cuenta con cursos de: \nDesarrollo e Ingenieria\nMarketing \nDiseño y UX \nNegocios y emprendimiento \nProducción Audiovisual \nCrecimiento profesional')
     
 
