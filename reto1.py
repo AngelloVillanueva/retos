fe5b6edd68dd608c6576153344632378a0de7dee
Angello Villanueva <angellovillanuevaind@gmail.com>
2020-06-13 22:05:15 -0400

    Agregar a archivo reto1.py el reto #7 donde se incluye la edad

--- reto1.py
+++ reto1.py
@@ -1,7 +1,10 @@
 def name():
     nombre = str(input('¿Cual es tu nombre?: '))
     apellido = str(input('¿Cual es tu Apellido?: '))
-    print('Hola, {} {}'.format(nombre, apellido))
+    edad = int(input('¿Cual es tu edad: '))
+    edad_ap = edad -1 
+    edad_pa = edad +1
+    print('Hola, {} {}, el año pasado tenias {} años y el próximo cumpliras {} años'.format(nombre, apellido, edad_ap, edad_pa))
 
     print('Platzi cuenta con cursos de: \nDesarrollo e Ingenieria\nMarketing \nDiseño y UX \nNegocios y emprendimiento \nProducción Audiovisual \nCrecimiento profesional')
     
