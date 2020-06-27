55c20f4b8814e1284324e93eae01c3854aa3242c
Angello Villanueva <angellovillanuevaind@gmail.com>
2020-06-27 15:26:16 -0400

    Cambio de variables más declarativas, ademas de generar el inicializador para consola y definir num_1 y num_2

--- segundo_reto/reto_01.py
+++ segundo_reto/reto_01.py
@@ -1,10 +1,15 @@
-def mayormenor(x,y):
+def mayormenor(num_1,num_2):
     try:
-        if x = y:
-            print(f'{x} y {y} son iguales')
-        elif x > y:
-            print(f'{x} es mas grande que {y}')
+        if num_1 == num_2:
+            print(f'{num_1} y {num_2} son iguales')
+        elif num_1 > num_2:
+            print(f'{num_1} es mayor que {num_2}')
         else:
-            print(f'{y} es mayor a {x}')
+            print(f'{num_1} es menor que {num_2}')
     except:
-        print('Error, ingresa un numero')
+        print('Error, ingresa un numero valido')
+
+if __name__ == '__main__':
+    num_1 = float(input('Escribe el un numero: '))
+    num_2 = float(input('Escribe el segundo numero: '))
+    mayormenor(num_1,num_2)
