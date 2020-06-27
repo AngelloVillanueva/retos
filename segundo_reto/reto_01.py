41815d727ce55f89e8934ab4d83d57bed47dcdc8
Angello Villanueva <angellovillanuevaind@gmail.com>
2020-06-27 15:16:04 -0400

    Inicio del reto 1 del segundo reto, elaboraciónn de función

---
+++ segundo_reto/reto_01.py
@@ -0,0 +1,10 @@
+def mayormenor(x,y):
+    try:
+        if x = y:
+            print(f'{x} y {y} son iguales')
+        elif x > y:
+            print(f'{x} es mas grande que {y}')
+        else:
+            print(f'{y} es mayor a {x}')
+    except:
+        print('Error, ingresa un numero')
